af7a52ecb7c2d63a2924a59bd99170735f0f2bae
gaotianpeng <gtpsydm@gmail.com>
2021-10-18 10:01:25 +0800

    add console log

--- main.cpp
+++ main.cpp
@@ -1,11 +1,17 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <iostream>
+
+using namespace std;
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    cout << "hello ffmpeg!!!!" << endl;
     MainWindow w;
 	w.setWindowTitle("DevTools!");
+    w.resize(800, 600);
     w.show();
     return a.exec();
 }
